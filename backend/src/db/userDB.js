ef284ea7d9fcea9c539b1d39b0d410b7cf006066
paulowos <paulowos@gmail.com>
2023-07-25 09:00:16 -0300

    teste: user creation

--- backend/src/db/userDB.js
+++ backend/src/db/userDB.js
@@ -1,9 +1,9 @@
-const { randomUUID } = require('crypto');
+const crypto = require('crypto');
 const argon2 = require('argon2');
 const connection = require('./connection');
 
 const createUser = async ({ name, email, password }) => {
-  const id = randomUUID();
+  const id = crypto.randomUUID();
   const hash = await argon2.hash(password);
   await connection.execute(`INSERT INTO users (id, name, email, password) 
   VALUES (?, ?, ?, ?)`, [id, name, email, hash]);
--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -4,10 +4,11 @@ const chaiHttp = require('chai-http');
 const sinon = require('sinon');
 
 chai.use(chaiHttp);
-const argon2 = require('argon2');
 const app = require('../src/app');
 
+const argon2 = require('argon2');
 const connection = require('../src/db/connection');
+let crypto = require('crypto');
 
 const { expect } = chai;
 
@@ -63,6 +64,32 @@ describe('Testando rota /user', function () {
       expect(response.status).to.equal(401);
       expect(response.body).to.have.property('error');
       expect(response.body.error).to.equal('Email inválido/Não cadastrado');
+    });
+  });
+
+
+  describe('Testando método POST "/create"', function () {
+
+    it('Deve conseguir criar um usuário', async function () {
+      sinon.stub(crypto, 'randomUUID').returns('teste');
+      sinon.stub(argon2, 'hash').resolves('teste');
+      sinon.stub(connection, 'execute').resolves();
+
+      const response = await chai.request(app)
+        .post('/user/create')
+        .send({
+          name: 'teste',
+          email: 'teste@teste.com',
+          password: 'teste123456',
+
+        });
+
+      expect(response.status).to.equal(201);
+      expect(response.body).to.have.property('id');
+      expect(response.body.id).to.equal('teste');
+
+
+
     });
   });
 });
